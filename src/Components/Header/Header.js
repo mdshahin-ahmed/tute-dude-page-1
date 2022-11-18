abbaa27330742a06a151b01e438e3f6901577c09
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-11-18 20:05:39 +0600

    Done friends referred page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+import "./Header.css";
 import brandImage from "../../images/image_1.png";
 
 const Header = () => {
   return (
     <>
       <Navbar bg="white" variant="light">
-        <Container>
+        <Container fluid>
           <Navbar.Brand href="#home">
             <img src={brandImage} alt="" />
           </Navbar.Brand>
           <Nav className="navLink ms-auto">
-            <Nav.Link className="d-none d-md-block" href="#home">
+            <Nav.Link className="menuLink d-none d-md-block" href="#home">
               My Assignment
             </Nav.Link>{" "}
-            <Nav.Link className="d-none d-md-block" href="#home">
+            <Nav.Link className="menuLink d-none d-md-block" href="#home">
               Chat with Mentor
             </Nav.Link>{" "}
-            <div className="profile d-flex align-self-center">
-              <i className="fa-solid fa-user"></i>
+            <div className="profile d-flex">
+              <div className="userLogoWrap d-none d-md-flex">
+                <i className="fa-solid fa-user"></i>
+              </div>
               <span>ProfileName</span>
-              <i className="fa-solid fa-angle-down"></i>
+              <i className="profileArrow fa-solid fa-angle-down"></i>
             </div>
           </Nav>
         </Container>
